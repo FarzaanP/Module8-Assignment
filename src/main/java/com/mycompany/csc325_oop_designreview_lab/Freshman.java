60fdd82085c532c0e92ba8daf1849031c4e610f7
Farzaan Patwary <patwfk@farmingdale.edu>
2026-04-05 13:06:12 -0400

    added the toString method in Freshman so it returns the Freshman info in a string. finished todo 10

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Freshman.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Freshman.java
@@ -5,4 +5,10 @@ public class Freshman extends Student {
     public Freshman(String name, short age, int credits) {
         super(name, age, credits);
     }
+    // returns freshman info in a string
+    @Override
+    public String toString() {
+        return "Freshman{name='" + getName() + "', age=" + getAge()
+                + ", credits=" + getCredits() + ", gpa=" + getGpa() + "}";
+    }
 }
